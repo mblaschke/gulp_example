84245d66cbeb7d788ca3d4e06601dadcd2ead155
Daniel Goerz <dlg@lightwerk.com>
2016-03-17 18:00:45 +0100

    [BUGFIX] Switch to lower case for gulp_tasks folder in comment

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -2,6 +2,6 @@
 var requireDir = require('require-dir'),
     dir = requireDir('./gulp_tasks');
 
-// The actual tasks are configured in external files under ./GulpTasks/
+// The actual tasks are configured in external files under ./gulp_tasks/
 // https://github.com/gulpjs/gulp/blob/master/docs/recipes/split-tasks-across-multiple-files.md
-// For the default task, that gets triggered with the bare 'gulp' command, see ./GulpTasks/default.js
+// For the default task, that gets triggered with the bare 'gulp' command, see ./gulp_tasks/default.js
